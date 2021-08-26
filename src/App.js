d8e7f9c3ea9b89584099daa824234e21c3ab7f9d
Prabhav <prabhavtewari@gmail.com>
2021-08-26 22:46:23 +0530

    main page header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import header from './Header';
+import rarrow from './images/icon-arrow.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <div className="content">
-        <h1>Hello 1</h1>
+      <div className="headerwrap">
+        <div className="header">
+          <form action="">
+          IP Address Tracker
+          <div className="submitrow">
+          <input className="ipinput" type="text" />
+          <button type="submit"  className="submitarrow">
+            <img src={rarrow} alt="" srcset="" />
+          </button>
+          </div>
+          </form>
+          </div>
       </div>
     </div>
   );
